86124b6ffca1f04923777cdef467735675f5e636
mateusz7812 <40182359+mateusz7812@users.noreply.github.com>
2019-02-17 10:22:16 +0100

    refactor

--- src/Requester.kt
+++ src/Requester.kt
@@ -2,8 +2,8 @@ import java.net.URL
 import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.InputStreamReader
-import java.lang.Exception
 import java.net.HttpURLConnection
+import java.util.concurrent.TimeUnit
 
 
 class Requester(host: String = "localhost", port: Int = 8080){
@@ -18,52 +18,47 @@ class Requester(host: String = "localhost", port: Int = 8080){
         conn.disconnect()
     }
 
-    fun make_request(data: String): String? {
-        open()
-        try{
-            conn.requestMethod = "POST"
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
-            conn.setRequestProperty("Content-Length", Integer.toString(data.toByteArray().size))
-            conn.setRequestProperty("Content-Language", "en-US")
-            conn.useCaches = false
-            conn.doOutput = true
-            val wr = DataOutputStream(
-                conn.outputStream
-            )
-            wr.writeBytes(data)
-            wr.close()
-
-            val instream = conn.inputStream
-            val rd = BufferedReader(InputStreamReader(instream))
-            val response = StringBuilder() // or StringBuffer if Java version 5+
-            var line: String?
-            while (true) {
-                line = rd.readLine()
-                if(line == null){
-                    break
-                }
-                response.append(line)
-                response.append('\r')
-            }
-            rd.close()
-            return response.toString()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return null
-        }finally {
-            close()
-        }
-
+    fun prepare_request(data: String){
+        conn.requestMethod = "POST"
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+        conn.setRequestProperty("Content-Length", Integer.toString(data.toByteArray().size))
+        conn.setRequestProperty("Content-Language", "en-US")
+        conn.useCaches = false
+        conn.doOutput = true
     }
 
+    fun send_request(data: String) {
+        val wr = DataOutputStream(
+            conn.outputStream
+        )
+        wr.writeBytes(data)
+        wr.close()
+        return
+    }
 
+    fun get_response(): String {
+        val instream = conn.inputStream
+        val rd = BufferedReader(InputStreamReader(instream))
+        val response = rd.readLines().toString()
+        rd.close()
+        return response
+    }
+
+    fun make_request(data: String): String? {
+        open()
+        prepare_request(data)
+        send_request(data)
+        val response = get_response()
+        close()
+        return response
+    }
 }
 
 fun main(){
     val req = Requester()
     while(true){
-
-        req.make_request("[\"Hello\"]")
-
+        val response = req.make_request("[\"Hello\"]")
+        print(response)
+        TimeUnit.SECONDS.sleep(5)
     }
 }
